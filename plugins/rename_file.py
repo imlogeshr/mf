1583a846e14f7d1c784840a1373e510d4ed38cbd
surlogu <45895919+surlogu@users.noreply.github.com>
2020-06-06 14:50:29 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -6,6 +6,7 @@
 #
 # All rights reserved.
 
+import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
