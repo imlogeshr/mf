c745807d0043053ab2cd5988e9c6d0ced518bbb2
surlogu <45895919+surlogu@users.noreply.github.com>
2020-08-03 22:29:11 +0530

    Update convert2file.py

--- plugins/convert2file.py
+++ plugins/convert2file.py
@@ -25,7 +25,6 @@ else:
 from translation import Translation
 
 import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram
@@ -39,7 +38,7 @@ from PIL import Image
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["convert2file"]))
 async def convert2file(bot, update):
-    if update.from_user.id in Config.BANNED_USERS:
+    if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
             message_ids=update.message_id,
