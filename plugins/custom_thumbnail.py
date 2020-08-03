390bfdb6e55f487b272812b9124cef402cd4fab5
surlogu <45895919+surlogu@users.noreply.github.com>
2020-08-03 19:41:00 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -30,6 +30,8 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
 
+import database.database as sql
+
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["generatecustomthumbnail"]))
 async def generate_custom_thumbnail(bot, update):
@@ -103,6 +105,7 @@ async def save_photo(bot, update):
         # create download directory, if not exist
         if not os.path.isdir(download_location):
             os.makedirs(download_location)
+        await sql.df_thumb(update.from_user.id, update.message_id)    
         await bot.download_media(
             message=update,
             file_name=download_location
@@ -110,6 +113,7 @@ async def save_photo(bot, update):
     else:
         # received single photo
         download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+        await sql.df_thumb(update.from_user.id, update.message_id)
         await bot.download_media(
             message=update,
             file_name=download_location
@@ -133,6 +137,7 @@ async def delete_thumbnail(bot, update):
     TRChatBase(update.from_user.id, update.text, "deletethumbnail")
     download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
     try:
+        await sql.del_thumb(update.from_user.id)
         os.remove(download_location + ".jpg")
         # os.remove(download_location + ".json")
     except:
