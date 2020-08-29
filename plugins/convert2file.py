f420adcd7210a4bd543f06cd6be05abe93698b1d
surlogu <45895919+surlogu@users.noreply.github.com>
2020-08-29 20:32:29 +0530

    Update convert2file.py

--- plugins/convert2file.py
+++ plugins/convert2file.py
@@ -108,10 +108,11 @@ async def conv2doc(bot, update):
                 # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
             # try to upload file
             c_time = time.time()
+            current_file_name = os.path.join(the_real_download_location, file_name)
             await bot.send_document(
                 chat_id=update.chat.id,
-                document=the_real_download_location,
-                caption=description,
+                document=current_file_name,
+                caption=file_name + description,
                 #reply_markup=reply_markup,
                 thumb=thumb_image_path,
                 reply_to_message_id=update.reply_to_message.message_id,
