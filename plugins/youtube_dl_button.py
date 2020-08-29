480dfefb930df48f943b9792c77900618e5c46b7
surlogu <45895919+surlogu@users.noreply.github.com>
2020-08-29 21:11:27 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -38,6 +38,7 @@ from hachoir.parser import createParser
 # https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
 from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
+from helper_funcs.database import *
 
 
 async def youtube_dl_call_back(bot, update):
