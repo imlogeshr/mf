3fa1d7f4275ab501449c93595b7b0954a06c83bd
surlogu <45895919+surlogu@users.noreply.github.com>
2020-08-30 20:39:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = pyrogram.Client(
-        "AnyDLBot",
+        "AsEnDL",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
