27c73b28a5a90082228cd4710e6eced50b863bbe
surlogu <45895919+surlogu@users.noreply.github.com>
2020-08-31 12:23:34 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -125,7 +125,7 @@ async def rename_doc(bot, update):
                 chat_id=update.chat.id,
                 document=new_file_name,
                 thumb=thumb_image_path,
-                caption=file_name + description + Config.CH_CAPTION,
+                caption=file_name.rsplit('.', 1)[0] + Config.CH_CAPTION,
                 # reply_markup=reply_markup,
                 reply_to_message_id=update.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
