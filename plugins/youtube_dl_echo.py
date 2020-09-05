639a1d07fc35c43f326675e220aaed9c4f797fd5
surlogu <45895919+surlogu@users.noreply.github.com>
2020-09-05 12:02:16 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -263,7 +263,7 @@ async def echo(bot, update):
         # logger.info(reply_markup)
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION,
+            text=Translation.FORMAT_SELECTION.format("description"),
             reply_markup=reply_markup,
             parse_mode="html",
             reply_to_message_id=update.message_id
