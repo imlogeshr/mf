552089891808881172a4b6f031b7b5401cb00ba8
surlogu <45895919+surlogu@users.noreply.github.com>
2020-09-05 12:44:33 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -111,7 +111,6 @@ async def youtube_dl_call_back(bot, update):
         description = custom_file_name
     else:
         description = response_json["fulltitle"][0:1021]
-    fname = "description"
         # escape Markdown and special characters
     tmp_directory_for_each_user = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
     if not os.path.isdir(tmp_directory_for_each_user):
