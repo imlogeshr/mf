471b2c60c5f774926b56b134e0627d198b0225b1
surlogu <45895919+surlogu@users.noreply.github.com>
2020-09-07 22:57:47 +0530

    Update convert2file.py

--- plugins/convert2file.py
+++ plugins/convert2file.py
@@ -34,7 +34,7 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 # https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
-*from helper_funcs.database import *
+#from helper_funcs.database import *
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["conv2doc"]))
 async def conv2doc(bot, update):
